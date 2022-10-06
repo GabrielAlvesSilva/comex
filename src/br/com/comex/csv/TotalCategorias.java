92fdba817f29b814a4edfb864d96b97e51b71e7d
GabrielAlvesSilva <gsbj11111@gmail.com>
2022-10-06 14:18:50 -0300

    Calculadora de Total Categorias

---
+++ src/br/com/comex/csv/TotalCategorias.java
@@ -0,0 +1,25 @@
+package br.com.comex.csv;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TotalCategorias {
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		LeitorPedidosCsv leitorpedidos = new LeitorPedidosCsv();
+		List<PedidoCsv> pedidosCsv = leitorpedidos.LerPedidoCsv();
+		List<String> categorias = new ArrayList<String>();
+		for(int i = 0; i < pedidosCsv.size(); i++)
+		{
+			String categoria = pedidosCsv.get(i).getCategoria();
+			if(categorias.contains(categoria)) {
+				
+			}else {
+				categorias.add(categoria);
+			}
+		}
+		System.out.println("Total de Categorias: " + categorias.size());
+		}
+	
+}
