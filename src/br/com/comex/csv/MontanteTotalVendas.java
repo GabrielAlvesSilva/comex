8bde50735b2e60b2561954f919c2237e9b13099f
GabrielAlvesSilva <gsbj11111@gmail.com>
2022-10-06 14:35:29 -0300

    Soma total do Montante de vendas

---
+++ src/br/com/comex/csv/MontanteTotalVendas.java
@@ -0,0 +1,20 @@
+package br.com.comex.csv;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class MontanteTotalVendas {
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		LeitorPedidosCsv leitorpedidos = new LeitorPedidosCsv();
+		List<PedidoCsv> pedidosCsv = leitorpedidos.LerPedidoCsv();
+		double soma = 0;
+		for(int i = 0; i < pedidosCsv.size(); i++)
+		{
+			double mult = 0;
+			mult = Double.parseDouble(pedidosCsv.get(i).getPreco()) * Double.parseDouble(pedidosCsv.get(i).getQuantidade()); 
+			soma = soma + mult;
+		}
+		System.out.println("Montante de vendas: " +soma);
+	}
+}
