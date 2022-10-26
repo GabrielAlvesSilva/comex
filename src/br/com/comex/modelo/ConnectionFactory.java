f47b115f12c286f08bc2beef9a61bcdb34bdff6b
GabrielAlvesSilva <gsbj11111@gmail.com>
2022-10-26 10:08:43 -0300

    Atividade Executar container com Oracle

--- src/br/com/comex/modelo/ConnectionFactory.java
+++ src/br/com/comex/modelo/ConnectionFactory.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 	public Connection getConnection() {
 		try {
-			return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "comex", "comex");
+			return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1522:xe", "comex", "comex");
 		}catch(SQLException e){
 			throw new RuntimeException(e);
 		}
