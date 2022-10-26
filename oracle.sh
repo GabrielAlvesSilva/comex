7d453356d7401a72c807372d0af5ac5e1dcbc19f
GabrielAlvesSilva <gsbj11111@gmail.com>
2022-10-26 15:31:50 -0300

    Atividade Volumes no container do Oracle

--- oracle.sh
+++ oracle.sh
@@ -1,1 +1,5 @@
 docker run -d -p 1522:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
+
+docker volume create oracle-volume
+docker run -d -p 1522:1521 -v oracle-volume -e  ORACLE_PASSWORD=admin gvenzl/oracle-xe
+docker container logs -f festive_williamson
